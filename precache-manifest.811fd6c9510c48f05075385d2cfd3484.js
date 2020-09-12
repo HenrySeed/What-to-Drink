ff39f27370b0c67d2f4227202ca931df193c2590
Henry Seed <the.seedh@gmail.com>
2020-09-12 22:30:14 +1200

    Updates

---
+++ precache-manifest.811fd6c9510c48f05075385d2cfd3484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b661ad7f2eb7ba4dea23be39d3b9c0c",
+    "url": "/What-to-Drink/index.html"
+  },
+  {
+    "revision": "610f9420ead3c952b766",
+    "url": "/What-to-Drink/static/css/main.aa94a14e.chunk.css"
+  },
+  {
+    "revision": "5bdcf4d6ce28b579ad6d",
+    "url": "/What-to-Drink/static/js/2.628c5727.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/What-to-Drink/static/js/2.628c5727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610f9420ead3c952b766",
+    "url": "/What-to-Drink/static/js/main.0beffd04.chunk.js"
+  },
+  {
+    "revision": "6a9f5c728c4d3d6f3d3c",
+    "url": "/What-to-Drink/static/js/runtime-main.62dddeb8.js"
+  },
+  {
+    "revision": "91bea976121354a81128e3278a0df9fd",
+    "url": "/What-to-Drink/static/media/bg_1.91bea976.jpg"
+  }
+]);
